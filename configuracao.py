1756741643f37c92dd2cc6aee071b6738ff5c78a
bobboyms <bobboyms@gmail.com>
2018-12-17 21:14:27 -0400

    update

--- configuracao.py
+++ configuracao.py
@@ -1,7 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123456789@localhost/teste"
+db = SQLAlchemy(app)
+
 app.debug = True
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.nome
+
+cliente = Cliente()
+cliente.nome = "pedro jose"
+
+db.session.add(cliente)
+db.session.commit()
